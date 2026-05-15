05c4e0ee9b7f21e7ca2cd7545c864ec2666a5c23
Gabriel Funes <gabriel.antonio.funes.s@gmail.com>
2026-05-15 17:12:03 -0300

    agrega enums para promociones, requests y priceChange

---
+++ PriceOrchestrator.Api/Entities/Enums/PriceChangeRequestStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace PriceOrchestrator.Api.Entities.Enums
+{
+    public enum PriceChangeRequestStatus
+    {
+        Pending = 1,    // Esperando ser aplicado
+        Applied = 2,    // Ya se aplicó al precio actual
+        Expired = 3,    // Venció sin aplicarse
+        Cancelled = 4   // Cancelado manualmente
+    }
+}
---
+++ PriceOrchestrator.Api/Entities/Enums/PromotionType.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceOrchestrator.Api.Entities.Enums
+{
+    public enum PromotionType
+    {
+        Percentage = 1,    // 20% off
+        FixedAmount = 2,   // $50 off
+        BuyXGetY = 3       // 2x1, 3x2
+    }
+}
